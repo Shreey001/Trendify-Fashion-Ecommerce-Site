ebc32b3a2e749f2ccb9112072afd2826a88305c6
Shreejan <shreejanid123@gmail.com>
2025-02-09 12:41:38 +0545

    fix SearchBar input handling and enhance filtering in Collection page with search functionality

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -6,7 +6,7 @@ import ProductItem from '../components/ProductItem';
 
 const Collection = () => {
 
-  const { products } = useContext(ShopContext);
+  const { products,search,showSearch } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
@@ -34,7 +34,13 @@ const Collection = () => {
 
   const applyFilter = (e) => { 
 
+    
     let productsCopy = products.slice();
+    
+    if (showSearch && search) {
+      productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+    }
+
 
     if (category.length > 0) {
       productsCopy = productsCopy.filter(item => category.includes(item.category));
@@ -72,7 +78,7 @@ setFilterProducts(productsCopy);
 
   useEffect(() => { 
     applyFilter();
-  }, [category,subCategory]);
+  }, [category,subCategory,search,showSearch]);
 
   useEffect(() => {
 
