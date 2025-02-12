0488fb5a621dc0147fbc65a34cfbeace4d5254eb
Shreejan <shreejanid123@gmail.com>
2025-02-12 16:37:11 +0545

    Set up backend server with MongoDB and Cloudinary integration

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config' 
+import connectDB from './config/mongodb.js';
+import connectCloudnary from './config/cloudnary.js';
+
+// App config
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB()
+connectCloudnary()
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Api endpoints
+app.get('/', (req, res) => {
+res.send("API Working")
+ 
+});
+
+app.listen(port, ()=>console.log('Server started on PORT: ' + port));
