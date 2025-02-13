dea721612dac3645b275757443ad0c0ac59999b1
Shreejan <shreejanid123@gmail.com>
2025-02-13 21:36:11 +0545

    product add with images done with no error finally after 3 hr headache

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config' 
 import connectDB from './config/mongodb.js';
-import connectCloudnary from './config/cloudnary.js';
+import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/userRoute.js';
+import productRouter from './routes/productRoute.js';
 
 // App config
 const app = express();
@@ -13,7 +14,7 @@ const app = express();
 
 const port = process.env.PORT || 4000;
 connectDB()
-connectCloudnary()
+connectCloudinary()
 
 // Middleware
 app.use(cors());  // Optional if dealing with frontend requests
@@ -22,6 +23,7 @@ app.use(express.urlencoded({ extended: true })); // ✅ Handles form data
 
 //api endpoint
 app.use('/api/user', userRouter)
+app.use('/api/product',productRouter)
 
 
 
