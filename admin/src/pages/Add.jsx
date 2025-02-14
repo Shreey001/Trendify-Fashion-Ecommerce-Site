bef81f15b317e94432c64a29be6dfbf5354e6f71
Shreejan <shreejanid123@gmail.com>
2025-02-14 18:16:11 +0545

    clear fields after product added (done with no error)

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { assets } from '../assets/assets';
 import axios from 'axios';
 import { backendUrl } from '../App';
+import { toast } from'react-toastify';
 
 const Add = ({token}) => {
   const [image1, setImage1] = useState(null);
@@ -38,12 +39,28 @@ const Add = ({token}) => {
       // Make API call to add product
       const response = await axios.post(backendUrl + "/api/product/add", formData,{headers:{token}});
     
-      
+      if (response.data.success) {
+        toast.success(response.data.message)
+        setName('');
+
+        setDescription('');
+        setImage1(false)
+        setImage2(false)
+        setImage3(false)
+        setImage4(false)
+        setPrice('');
+
+
+      }
+      else {
+        toast.error(response.data.message)
+      }
 
 
 
     } catch (error) {
       console.error("Error uploading product:", error);
+      toast.error(error.response?.data?.message || 'An error occurred')
     }
   };
 
