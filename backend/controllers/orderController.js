b2b70502f208528547ce2fe102ca8c090a419349
Shreejan <shreejanid123@gmail.com>
2025-02-15 17:40:24 +0545

    create controller functions dummys(no error)

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,53 @@
+
+
+// Placing orders using COD method
+
+const placeOrder = async (req,res) => {
+    
+
+
+}
+
+
+
+
+//placing order using stripe payment
+
+const placeOrderStripe = async (req,res) => {
+
+
+}
+
+
+
+//placing order using razorpay payment
+
+const placeOrderRazorpay = async (req,res) => {
+
+
+}
+    
+    
+
+// All orders data for admin panel
+
+const allOrders = async (req,res) => {
+
+
+}
+
+
+//User Order data for frontend
+
+const userOrders = async (req,res) => {
+
+
+
+}
+
+//Update order status from admin panel
+
+const updateStatus = async (req,res) => {
+
+
+}
